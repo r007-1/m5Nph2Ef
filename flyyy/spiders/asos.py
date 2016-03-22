8d68af5f6484e17a86283f81a10da21d577226b0
r007-1 <lovette@nuyolk.com>
2016-03-23 00:32:52 +0800

    Added files via upload

--- flyyy/spiders/asos.py
+++ flyyy/spiders/asos.py
@@ -3,8 +3,11 @@ from lxml import etree
 import requests
 import scrapy
 from flyyy.items import NuyolkItem
-import random
 import time
+import random
+import datetime
+import pandas
+
 
 class Asos(scrapy.Spider):
     name = "asos"
@@ -12,10 +15,10 @@ class Asos(scrapy.Spider):
     start_urls = []
     sitemaps = []
 
-    sitemap_main = ["http://www.asos.com/au/sitemap.ashx"]
+    sitemap_main = ["http://us.asos.com/sitemap.ashx"]
     main_tags = bs(requests.get(sitemap_main[0]).text, "lxml").find_all("sitemap")
     for main_tag in main_tags:
-      sitemaps.append(main_tag.findNext("loc").text)
+        sitemaps.append(main_tag.findNext("loc").text)
 
     for sitemap in sitemaps:
         tags = bs(requests.get(sitemap).text, "lxml").find_all("url")
@@ -24,76 +27,101 @@ class Asos(scrapy.Spider):
             if '?iid=' in prod_link:
                 start_urls.append(prod_link)
     start_urls = start_urls[0:25]
-    au = 0
-    for url in start_urls:
-      if 'au.asos' in url:
-        au +=1
 
     def parse(self, response):
         datetime = int(str(int(time.time()*100))) #Don't change!
         random.seed(1412112 + datetime) #Don't change!
 
         item = NuyolkItem() #Don't change!
-        item['brand'] = "" #Needs post-processing!
+        item['_id'] = str(datetime) + str(int(random.uniform(100000, 999999))) #Don't change!
 
-        item['cat_code'] = ""
-        item['cat_1'] = "" #deprecate
-        item['cat_2'] = "" #deprecate
-        item['cat_3'] = "" #deprecate
-
-        item['currency'] = response.selector.xpath('//div[@class="currency"]/span[@class="code"]/text()').extract()[0]
-        item['currency_code'] = response.selector.xpath('//*[@class="currency_list"]//*[@selected="selected"]/text()').extract()
-        item['merchant_country']
-        item['language']
-
-        item['date_added'] = [unicode(str(time.strftime("%d/%m/%Y %H:%M:%S")), "utf-8")]
-
-        item['is_available'] = True #Don't change! #Fix later!
         item['affiliate_partner'] = "viglink"
-
-        item['prod_id'] = int(str(datetime) + str(int(random.uniform(100000, 999999)))) #Don't change!
+        item['brand'] = response.selector.xpath('//div[@id = "productTabs"]/div[@id="ctl00_ContentMainPage_brandInfoPanel"]/a[1]/strong/text()').extract()[0]
+        item['long_desc'] = " | ".join(response.selector.xpath('//div[@id="ctl00_ContentMainPage_productInfoPanel"]/ul/li/text()').extract())
+        item['short_desc'] = response.selector.xpath('//div[@class="title"]/h1/span[@class="product_title"]/text()').extract()[0]
         item['product_link'] = response.selector.xpath('//head/link[@rel="canonical"]/@href').extract()[0]
 
-        item['merchant_prod_id'] = response.selector.xpath('//div[@id="product-detail"]/div[@class="product-name"]/@id').extract()[0][2:]
-        item['merchant_id'] = "3O056R"
+        item['cat_1'] = ""
+        item['cat_2'] = ""
+        item['cat_3'] = ""
+        item['cat_code'] = ""
 
+        item['date_added'] = [unicode(str(time.strftime("%d/%m/%Y %H:%M:%S")), "utf-8")]
+        item['date_last_updated'] = [unicode(str(time.strftime("%d/%m/%Y %H:%M:%S")), "utf-8")]
 
-        item['short_desc'] = response.selector.xpath('//div[@id="product-detail"]/div[@class="product-name"]/h1/text()').extract()[0]
-        item['long_desc'] = response.selector.xpath('//div[@itemprop="description"]/p/text()').extract()[0].strip()
-        item['primary_color'] = "" #later
-
-
-        #If item is on sale,
-
-        try:
-            if (int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/span[@class="sale"]/text()').extract()[0][1:])) != int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/span[@class="retail"]/text()').extract()[0][1:]))):
-                orig = int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/span[@class="retail"]/text()').extract()[0][1:]))
-                sale = int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/span[@class="sale"]/text()').extract()[0][1:]))
-                item['price_orig'] = orig
-                item['price_sale'] = sale
-                item['price_perc_discount'] = int(100-100*(sale/orig))
-                item['price'] = item['price_sale']
-            else:
-                item['price_orig'] = int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/span[@class="retail"]/text()').extract()[0][1:]))
-                item['price'] = item['price_orig']
-        except IndexError:
-            item['price_orig'] = int(float(response.selector.xpath('//div[@class="product-name"]/span[@id="product-price"]/span/text()').extract()[0][1:]))
-            item['price'] = item['price_orig']
-
-        item['image_urls'] = response.selector.xpath('//ul[@id="image-carousel"]/li/a/@href').extract()
+        item['image_urls'] = ""
         item['img_1'] = ""
         item['img_2'] = ""
         item['img_3'] = ""
         item['img_4'] = ""
         item['img_5'] = ""
 
-        item['mcats'] = "" #later #Do NLP predictions
+        try:
+            item['imglink_1'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage1"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_1'] = ""
+
+        try:
+            item['imglink_2'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage2"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_2'] = ""
+
+        try:
+            item['imglink_3'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage3"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_3'] = ""
+
+        try:
+            item['imglink_4'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage4"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_4'] = ""
+
+        try:
+            item['imglink_5'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage5"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_5'] = ""
+
+        try:
+            item['imglink_6'] = response.selector.xpath('//div[@id="ctl00_ContentMainPage_mainImage6"]/img/@src').extract()[0]
+        except IndexError:
+            item['imglink_6'] = ""
 
 
+        item['mcat_1'] = ""
+        item['mcat_2'] = ""
+        item['mcat_3'] = ""
+        item['mcat_4'] = ""
+        item['mcat_5'] = ""
+        item['mcat_code'] = ""
+
+        item['merchant'] = "Asos"
+        item['merchant_id']  = "IU95X3"
+        item['merchant_prod_id'] = response.selector.xpath('//span[@class="productcode"]/text()').extract()[0]
+
+        item['is_available'] = True #BOOLEAN
+        item['currency'] = response.selector.xpath('//div[@class="currency-list"]/select/option[@selected="selected"]/text()').extract()[0][-3:]
+        item['currency_symbol'] = response.selector.xpath('//div[@class="currency-list"]/select/option[@selected="selected"]/text()').extract()[0][0:response.selector.xpath('//div[@class="currency-list"]/select/option[@selected="selected"]/text()').extract()[0].find(" ")]
+
+        try:
+            if (int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPrice"]/text()').extract()[0][1:])) != int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPreviousPrice"]/text()').extract()[0][1:]))):
+                orig = int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPrice"]/text()').extract()[0][1:]))
+                sale = int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPreviousPrice"]/text()').extract()[0][1:]))
+                item['price_orig'] = orig
+                item['price_sale'] = sale
+                item['price_perc_discount'] = int(100-100*(sale/orig))
+                item['price'] = item['price_sale']
+                item['on_sale'] = TRUE #BOOLEAN
+            else:
+                item['price_orig'] = int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPrice"]/text()').extract()[0][1:]))
+                item['price'] = item['price_orig']
+        except IndexError:
+            item['price_orig'] = int(float(response.selector.xpath('//div[@class="product_price"]/span[@id="ctl00_ContentMainPage_ctlSeparateProduct_lblProductPrice"]/text()').extract()[0][1:]))
+            item['price'] = item['price_orig']
+            item['on_sale'] = FALSE #BOOLEAN
+
+        item['primary_color'] = ""
 
         tags = [str(item['brand']), str(item['short_desc']), str(item['long_desc'])] #str(" ".join(item['mcats'])),
         item['tags'] = " ".join(tags)
-
-
 
         yield item
