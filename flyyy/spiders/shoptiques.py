c2225bea2c17bf1f55828e3eaa38ac69cbe38a28
Lovette Regner <lovette@Jayhans-MacBook-Pro.local>
2017-03-13 13:57:33 +0800

    Removed test status for shoptiques

--- flyyy/spiders/shoptiques.py
+++ flyyy/spiders/shoptiques.py
@@ -21,7 +21,6 @@ class Shoptiques(scrapy.Spider):
 			url = tag.findNext("loc").text
 			start_urls.append(url)
 	#start_urls = start_urls[62050:]
-	start_urls = start_urls[0:10]
 	def parse(self, response):
 		#client = algoliasearch.Client("BTPCHYHQQY", "e68eda57aa7bd4b52dd27e9226dec21a")
 		#index = client.init_index('products')
